6b76ea753a521c46ca73364bdc5c6cb5550e97fd
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-01-14 20:18:48 +0200

    created errors function to show errors

--- arithmetic_formater.py
+++ arithmetic_formater.py
@@ -1,5 +1,11 @@
 numbers= ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
 def arithmetic_arranger(problems):
+  #create a functions for errors
+  def errors():
+    #limit is 5 problems:
+    if len(problems)>5:
+      return "Error: Too many problems"
+  errors()
   arranged_problems = " "
   #create a for loop to separate the elements in the list
   for problem in problems:
